fb3f7360f3b9e7903b2a040742aafd12640fbd23
jcjssl <jcjssl@jcjssl.cn>
2024-02-29 20:17:03 +0800

    Remove the necessary directory 'others'.

--- others/考试.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-chipo = pd.read_csv('D:\\@我的记录文件夹\\生产实习python深度学习\\chipotle.tsv',encoding = 'ANSI')
-min_sum_item = chipo.groupby('item_name')['quantity'].sum().min()
-# count = chipo.nunique('item_name')
